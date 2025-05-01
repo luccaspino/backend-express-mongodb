baa9b0dd5ba35b592830bf0109fd63454ed5bb3f
Luccas Pereira de Pino <luccaspino@icloud.com>
2025-05-01 02:38:54 +0000

    modified:   .env.example 	new file:   api/database/configdb.js 	new file:   api/index.js 	new file:   api/models/User.js 	new file:   docker-compose.yml 	modified:   package.json 	deleted:    src/index.js 	new file:   vercel.json

---
+++ api/index.js
@@ -0,0 +1,23 @@
+import express from "express";
+import dotenv from "dotenv";
+import db from "./database/configdb.js";
+import User from "./models/User.js";
+
+
+dotenv.config();
+db.connect();
+
+const app = express();
+
+
+app.use(express.json());
+
+
+app.get("/", (req, res) => {
+    res.send({message: 'Hello World!'});
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port http://localhost:${PORT}/`);
+});
