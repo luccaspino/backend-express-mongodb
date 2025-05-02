fa074d72689b8adaab652cfe5b1f9fd848089dfd
Luccas Pereira de Pino <luccaspino@icloud.com>
2025-05-02 22:38:56 +0000

    modified:   api/routes/user.route.js

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,17 +1,8 @@
 import express from 'express';
-
 import userController from '../controller/user.controller.js';
 
 const router = express.Router();
 
 router.post('/register',userController.register);
-
-
-const router = express.Router();
-
-router.post('register', async (req, res) => {
-
-});
-
 
 export default router;
