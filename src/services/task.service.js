537d52b1d047f561a0f76e1163d2b631d8958910
Luccas Pereira de Pino <luccaspino@icloud.com>
2025-05-19 11:30:35 -0300

    Update task.service.js

--- src/services/task.service.js
+++ src/services/task.service.js
@@ -5,11 +5,19 @@ const getTasksByUser = async (userId) => await Task.find({ userId });
 const getTaskById = async (id, userId) => await Task.findOne({ _id: id, userId });
 const updateTask = async (id, data, userId) => await Task.findOneAndUpdate({ _id: id, userId }, data, { new: true });
 const deleteTask = async (id, userId) => await Task.findOneAndDelete({ _id: id, userId });
+const toggleTaskCompletion = async (id, userId) => {
+  const task = await Task.findOne({ _id: id, userId });
+  if (!task) return null;
+  
+  task.completed = !task.completed;
+  return await task.save();
+};
 
 export default {
   createTask,
   getTasksByUser,
   getTaskById,
   updateTask,
-  deleteTask
+  deleteTask,
+  toggleTaskCompletion
 };
